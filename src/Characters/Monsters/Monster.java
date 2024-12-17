c422b035846a3b8264b3621e75bcb2610f9116e5
Ludo DENIS <ludv.denis@gmail.com>
2024-12-17 19:01:59 +0100

    change

--- src/Characters/Monsters/Monster.java
+++ src/Characters/Monsters/Monster.java
@@ -2,11 +2,13 @@ package Characters.Monsters;
 
 import Characters.Character;
 import Characters.Hero.Hero;
+import Characters.Monsters.Magician;
 import Weapons.Sword;
 import Weapons.WeaponType;
 
 
 public class Monster extends Character {
+	
 
 	public Monster(WeaponType weapon) {
 		Weapons.Sword Sword = new Sword();
@@ -15,7 +17,15 @@ public class Monster extends Character {
 
 	public WeaponType weakness;
 
- public Monster(WeaponType) {
-	 
+ public boolean Monster(Magician magician) {
+	 	 if (Hero && Monster) {
+			 if (Magician = True) {
+				System.out.println("Take your bottle !");
+				return true;
+				else {
+					System.out.println("Take your sword !");
+				}
+			}
+	 }
  }
 }
