afee609c5d79d3f8d25a04ee3af805c2b477e9c4
Ludo DENIS <ludv.denis@gmail.com>
2024-12-20 15:05:49 +0100

    rougeRoom

--- src/main/java/game/Dungeon/Dungeon.java
+++ src/main/java/game/Dungeon/Dungeon.java
@@ -7,23 +7,25 @@ import java.util.*;
 import game.Game;
 
 public class Dungeon implements PropertyChangeListener {
-    private final Room[] rooms;
+	private final Room[] rooms;
+	private static final String ANSI_RED = "\u001B[31m";
+	private static final String ANSI_RESET = "\u001B[0m";
 
 	public Dungeon(int numberRooms) {
 		// Initialization of rooms with their monster
-        rooms = new Room[numberRooms];
-		for(int i = 0; i < numberRooms; i++){
+		rooms = new Room[numberRooms];
+		for (int i = 0; i < numberRooms; i++) {
 			rooms[i] = new Room();
 		}
 	}
+
 	public List<Room> room;
 
-    public void letEnter(Hero hero) {
-		//System.out.println("-- game.Dungeon Monster --\n");
-		for(int i = 0 ; i < rooms.length ; i ++){
-			System.out.println("You are in the room " + (i + 1) + " with "+ hero.getHealthPoints()+ " health points");
+	public void letEnter(Hero hero) {
+		for (int i = 0; i < rooms.length; i++) {
+			System.out.println(ANSI_RED + "!--- You are in the room " + (i + 1) + " with " + hero.getHealthPoints() + " health points ---!" + ANSI_RESET);
 			rooms[i].enterRoom(hero);
-			if(!hero.isAlive()) {
+			if (!hero.isAlive()) {
 				System.out.println("Too bad you're dead");
 				Game.gameOver();
 				break;
@@ -33,9 +35,56 @@ public class Dungeon implements PropertyChangeListener {
 		if (hero.isAlive()) {
 			System.out.println("Congratulations! You have completed all the rooms, you find treasure and won the game!");
 		}
-    }
+	}
 
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 	}
 }
+
+
+
+
+
+
+//package game.Dungeon;
+//
+//import game.Characters.Hero.Hero;
+//import java.beans.PropertyChangeEvent;
+//import java.beans.PropertyChangeListener;
+//import java.util.*;
+//import game.Game;
+//
+//public class Dungeon implements PropertyChangeListener {
+//    private final Room[] rooms;
+//
+//	public Dungeon(int numberRooms) {
+//		// Initialization of rooms with their monster
+//        rooms = new Room[numberRooms];
+//		for(int i = 0; i < numberRooms; i++){
+//			rooms[i] = new Room();
+//		}
+//	}
+//	public List<Room> room;
+//
+//    public void letEnter(Hero hero) {
+//		//System.out.println("-- game.Dungeon Monster --\n");
+//		for(int i = 0 ; i < rooms.length ; i ++){
+//			System.out.println("!--- You are in the room " + (i + 1) + " with "+ hero.getHealthPoints()+ " health points ---!");
+//			rooms[i].enterRoom(hero);
+//			if(!hero.isAlive()) {
+//				System.out.println("Too bad you're dead");
+//				Game.gameOver();
+//				break;
+//			}
+//		}
+//		// Check if the hero has finished all rooms and is still alive
+//		if (hero.isAlive()) {
+//			System.out.println("Congratulations! You have completed all the rooms, you find treasure and won the game!");
+//		}
+//    }
+//
+//	@Override
+//	public void propertyChange(PropertyChangeEvent evt) {
+//	}
+//}
